a72307f03ebc413e0a327157de7c601736a78177
Uba_Yusuf <yusufabdurrachman3@gmail.com>
2025-04-05 10:39:29 +0700

    "Version 3"

--- sensor_suhu.py
+++ sensor_suhu.py
@@ -29,16 +29,6 @@ elif temp_type == "Normal":
     print("Penyirmana 5 menit")
   else:
     print("Perangkat Error")
-
-elif temp_type == "Hot":
-  if moisture_type == "Dry":
-    print("Penyiraman 15 menit")
-  elif moisture_type == "Moist":
-    print("Penyiraman 10 menit")
-  elif moisture_type == "Wet":
-    print("Penyirman 5 menit")
-  else:
-    print("Perangkat Error")
-    
+       
 else:
   print("perangkat Error")
